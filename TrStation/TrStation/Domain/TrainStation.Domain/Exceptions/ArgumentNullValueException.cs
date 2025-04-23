dea5c98f52f99fae067151f52e897f0e40dbefea
Илья <subbotin03ilya@gmail.com>
2025-04-23 23:04:21 +0700

    Проверка, проб

--- TrStation/TrStation/Domain/TrainStation.Domain/Exceptions/ArgumentNullValueException.cs
+++ TrStation/TrStation/Domain/TrainStation.Domain/Exceptions/ArgumentNullValueException.cs
@@ -9,5 +9,5 @@ namespace TrainStation.Domain.TrainStation.Domain.Exceptions
     public class ArgumentNullValueException(string paramName)
         : ArgumentNullException(paramName, $"Argument \"{paramName}\" value is null"); // ArgumentNullException, Исключение, которое создается при передаче пустой ссылки (Nothing в Visual Basic) методу, который не принимает ее как допустимый аргумент.
         // Наследование. Object ==> Exception ==> SystemException ==> ArgumentException ==> ArgumentNullException
-
+         
 }
